5d147909657420967e3f6477129e8621b2c73b95
Fanfaneee <fanie_bugenne@ens.univ-artois.fr>
2026-01-05 15:28:38 +0100

    first commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -30,6 +30,14 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
+      <Tabs.Screen
+        name="explore2"
+        options={{ title: 'Explore2', tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />, }}
+      />
+      <Tabs.Screen
+        name="explore3"
+        options={{ title: 'Explore3', tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />, }}
+      />
     </Tabs>
   );
 }
